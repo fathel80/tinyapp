9de319d078e2825099e6f7438d47527c4395f265
fathel mohamud <fathel.mohamud@gmail.com>
2021-12-08 20:04:37 -0500

    ok

--- express_server.js
+++ express_server.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 8080; // default port 
 
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({extended: true}));
 
 const generateRandomString = function() {
   return Math.random().toString(36).substr(2, 8);
@@ -12,8 +16,7 @@ const generateRandomString = function() {
     "b2xVn2": "http://www.lighthouselabs.ca/",
     "9sm5xK": "http://www.google.com/"
   };
-  const bodyParser = require("body-parser");
-  app.use(bodyParser.urlencoded({extended: true}));
+  
 
   app.get("/", (req, res) => {
     res.send("Hello!");
@@ -28,12 +31,13 @@ const generateRandomString = function() {
     });
 
     app.get("/urls", (req, res) => {
-      const templateVars = { urls: urlDatabase };
+      console.log(req.cookies.username)
+      const templateVars = { username: req.cookies.username, urls: urlDatabase };
       res.render("urls_index", templateVars);
     });
 
     app.get("/u/:shortURL", (req, res) => {
-      const longURL = urlDatabase[req.params.shortURL];
+      const longURL = urlDatabase[req.params.shortURL]["longURL"];
       res.redirect(longURL);
     });
 
@@ -42,7 +46,7 @@ const generateRandomString = function() {
       urlDatabase[shortURL] = req.body.longURL;
       res.redirect(`urls/${shortURL}`);
     });
-
+    
     app.get("/urls/new", (req, res) => {
       res.render("urls_new");
     });
@@ -52,7 +56,21 @@ const generateRandomString = function() {
       let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
       res.render("urls_show", templateVars);
     });
+    app.post("/urls/:shortURL/delete", (req, res) => {
+      const shortURL = req.params.shortURL;
+      delete urlDatabase[shortURL];
+      res.redirect("/urls");
+    });
+    app.post("/urls/:id" , (req, res) => {
+      res.redirect("/urls");
+    });
+    app.post("/login" , (req, res) => {
+      res.cookie("username", req.body.username)
+    res.redirect('/urls')
+    });
+
 
     app.listen(PORT, () => {
       console.log(`Example app listening on port ${PORT}!`);
     });
+    
